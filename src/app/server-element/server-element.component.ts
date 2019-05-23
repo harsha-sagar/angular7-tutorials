e318318cea9e9632969247c5a8dfba066297ee09
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-05-23 16:53:11 +0530

    demonstrating life-cycle starting with constructor

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {name: string, content: string, type: string};
 
-  constructor() { }
+  constructor() {
+    console.log('ServerElementComponent constructor called');
+  }
 
   ngOnInit() {
   }

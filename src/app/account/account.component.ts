077bd34aed700822bcfdc4d67923c9746c994b68
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-05-28 07:55:27 +0530

    demonstrating service inside a service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,23 +1,20 @@
 import { Component, Input, Output } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private accountService: AccountService){
+  constructor(private accountService: AccountService){
   }
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,20 +1,17 @@
 import { Component, Output } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService, private accountService: AccountService){
+  constructor(private accountService: AccountService){
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }

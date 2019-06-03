a1c0a4f4258f87dddbe474820768af28876aa2c8
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-06-03 18:14:25 +0530

    guarding for leaving the component

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
@@ -9,13 +11,17 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./edit-server.component.css']
 })
 
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -37,6 +43,18 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['..'], {relativeTo: this.route})
   }
 
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean{
+    if (!this.allowEdit){
+      return true;
+    } 
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Are you sure about discarding the changes?');
+    } else {
+      return true;
+    }
+  }
 }

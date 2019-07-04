371d338705fdbf5001e4f42b8e1d6f4230f625d7
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-07-04 13:02:07 +0530

    demonstrating another example for parameterised custom ppe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+
+export class FilterPipe implements PipeTransform {
+  transform(value: [], filtered: string, prop: string): any {
+    if (value.length === 0 || filtered === '') {
+      return value;
+    }
+    const resultArr = [];
+    for (const item of value) {
+      if (item[prop] === filtered) {
+        resultArr.push(item);
+      }
+    }
+    return resultArr
+  }
+}
